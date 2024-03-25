1389f1155c955c880bcd1ec11a94439734c79e93
Sambhaji Patil <135008610+Samcreates@users.noreply.github.com>
2024-03-26 00:02:05 +0530

    Update 11. HigherLower.py

--- 11. HigherLower.py
+++ 11. HigherLower.py
@@ -334,66 +334,66 @@ def clear_screen():
   else:
     _ = os.system('clear')
 
-game_end=False
-
-while game_end!=True:
-    clear_screen()
-    print(logo)
-    score=0
+def get_random_account():
+  """Get data from random account"""
+  return random.choice(data)
 
 
-    def get_random_account():
-        return random.choice(data)
+def format_data(account):
+  """Format account into printable format: name, description and country"""
+  name = account["name"]
+  description = account["description"]
+  country = account["country"]
+  return f"{name}, a {description}, from {country}"
 
-    def game(account1,account2):
-        print(f"\n\nCompare A: {account1['name']}, a {account1['description']}, from {account1['country']}.")
-        print(vs)
-        print(f"\Against B: {account2['name']}, a {account2['description']}, from {account2['country']}")
-        compare(account1,account2)
+
+def check_answer(guess, a_followers, b_followers):
+  """Checks followers against user's guess 
+  and returns True if they got it right.
+  Or False if they got it wrong."""
+  if a_followers > b_followers:
+    return guess == "a"
+  else:
+    return guess == "b"
+
+
+def game():
+  clear_screen()
+  print(logo)
+  score = 0
+  game_should_continue = True
+  account_a = get_random_account()
+  account_b = get_random_account()
+
+  while game_should_continue:
+    account_a = account_b
+    account_b = get_random_account()
+
+    while account_a == account_b:
+      account_b = get_random_account()
+
+    print(f"\n\nCompare A: {format_data(account_a)}.")
+    print(vs)
+    print(f"Against B: {format_data(account_b)}.")
+
+    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
     
-    def compare(account1,account2):
-        global score
-        if account1['follower_count']>account2['follower_count']:
-            winner=account1['name']
-        elif account1['follower_count']<account2['follower_count']:
-            winner=account2['name']
-        ans=input("\nWho has more followers? Type 'A' or 'B': ").lower()
-        if ans=='a':
-            if account1['name']==winner:
-                score=score+1
-                print(f"\nYou are right. Current score: {score}")
-                account1=get_random_account()
-                account2=get_random_account()
-                game(account1,account2)
-            else: 
-                print(f"\nYou are wrong. Final Score: {score}")
-                score=0
-                game_end=True
-        else:
-            if account2['name']==winner:
-                score=score+1
-                print(f"\nYou are right. Current score: {score}")
-                account1=get_random_account()
-                account2=get_random_account()
-                game(account1,account2)
-            else:
-                print(f"\nYou are wrong. Final Score: {score}")
-                score=0
-                game_end=True
+    if is_correct:
+      score += 1
+      print(f"You're right! Current score: {score}.")
+    else:
+      print(f"Sorry, that's wrong. Final score: {score}")
+      op = input("\nDo you want to play again? Type 'y' or 'n': ")
+      if op == 'y':
+        clear_screen()
+        score = 0
+      else:
+        game_should_continue = False
+        print("Goodbye!")
 
-    account1=get_random_account()
-    account2=get_random_account()
-    #if both account1 and account2 are same than this logic gets a new account
-    while account1 == account2:
-      account2 = get_random_account()
-    game(account1,account2)
 
-    print("Game Over")
-    op = input("\nDo you want to play again? Type 'y' or 'n': ")
-    if op.lower() != 'y':
-        print("Thank you for playing")
-        game_end=True
-    op = input("\nDo you want to play again? Type 'y' or 'n': ")
-    if op.lower() != 'y':
-        print("Thank you for playing")
-        game_end=True
+game()
